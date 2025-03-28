5846def0a2dadfd3ca90687ae1a7df7def19c918
Al-1109 <bertt141@gmail.com>
2025-03-28 20:20:34 +0100

    Update test deploy script

--- test_deploy.py
+++ test_deploy.py
@@ -1,4 +1,6 @@
 import datetime
+import socket
 
 print(f"Test deploy successful at: {datetime.datetime.now()}")
-print(f"Running from: {__file__}") 
+print(f"Running from: {__file__}")
+print(f"Hostname: {socket.gethostname()}") 
